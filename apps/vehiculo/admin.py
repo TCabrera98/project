96b35a59cacc0e29069bc911702b4fbc31680569
Valentin Cabrera <valentincabrera98@gmail.com>
2024-09-11 13:24:33 -0300

    add: modelos creados y registrados

--- apps/vehiculo/admin.py
+++ apps/vehiculo/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Vehiculo, VehiculoImagen, FormularioDeBusqueda
 
-# Register your models here.
+# Personalización del sitio de administración
+admin.site.site_header = "Cabrera Automotores"
+admin.site.site_title = "Cabrera Automotores"
+
+
+class VehiculoImagenInline(admin.TabularInline):
+    model = VehiculoImagen
+    extra = 1
+
+
+@admin.register(Vehiculo)
+class VehiculoAdmin(admin.ModelAdmin):
+    # Campos que se mostrarán en la lista de vehículos
+    list_display = ('marca', 'modelo', 'año',
+                    'kilometraje')
+
+    # Campos por los que se puede buscar en el panel de administración
+    search_fields = ('marca', 'modelo', 'año', 'kilometraje')
+
+    # Campos por los que se puede filtrar en el panel de administración
+    list_filter = ('marca', 'modelo', 'año', 'color')
+
+    # Agregar imágenes al panel de administración
+    inlines = [VehiculoImagenInline]
+
+
+@admin.register(FormularioDeBusqueda)
+class FormularioDeBusquedaAdmin(admin.ModelAdmin):
+    list_display = ('nombre_completo', 'numero', 'marca',
+                    'modelo', 'año')
+    search_fields = ('nombre_completo', 'numero', 'marca',
+                     'modelo', 'año')
--- apps/vehiculo/models.py
+++ apps/vehiculo/models.py
@@ -1,3 +1,76 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.core.validators import MinValueValidator
+
+# Opciones para el campo "condición" del vehículo
+CONDICION_VEHICULO = [
+    ("0km", "0km"),
+    ("Usado", "Usado"),
+]
+
+
+class Vehiculo(models.Model):
+    """ 
+    Clase que representa un vehículo con sus detalles principales, 
+    como marca, modelo, versión, año, kilometraje y condición (0km o usado).
+    """
+
+    marca = models.CharField(max_length=50)
+    modelo = models.CharField(max_length=50)
+    version = models.CharField(max_length=50)
+    año = models.IntegerField(null=True, blank=True)
+    kilometraje = models.IntegerField(null=True, default=0)
+    color = models.CharField(max_length=50, null=True, blank=True)
+    condicion = models.CharField(
+        max_length=50, choices=CONDICION_VEHICULO, default="0km")
+
+    def __str__(self):
+        return f"{self.marca} {self.modelo} {self.año}"
+
+    class Meta:
+        verbose_name = "Vehículo"
+        verbose_name_plural = "Vehículos"
+        indexes = [
+            # Indexar campos usados para búsquedas frecuentes
+            models.Index(fields=['marca', 'modelo', 'año']),
+        ]
+
+
+class VehiculoImagen(models.Model):
+    """ 
+    Clase para almacenar las imágenes asociadas a los vehículos. 
+    Cada imagen está vinculada a un vehículo a través de una relación de ForeignKey.
+    """
+
+    vehiculo = models.ForeignKey(
+        Vehiculo, related_name="imagenes", on_delete=models.CASCADE, null=True, blank=True
+    )
+    imagen = models.ImageField(upload_to="vehiculo/imagenes/")
+
+    def __str__(self):
+        return f"Imagen de {self.vehiculo}"
+
+
+class FormularioDeBusqueda(models.Model):
+    """
+    Clase que representa un formulario de búsqueda, donde los usuarios pueden buscar 
+    vehículos en función de criterios como marca, modelo, año, y precio.
+    """
+
+    nombre_completo = models.CharField(max_length=50)
+    # Se puede agregar una validación adicional para números de teléfono
+    numero = models.CharField(max_length=50)
+    marca = models.CharField(max_length=50)
+    modelo = models.CharField(max_length=50)
+    año = models.IntegerField(null=True, blank=True)
+    precio = models.DecimalField(
+        null=True, blank=True, decimal_places=2, max_digits=10, validators=[MinValueValidator(0.00)]
+    )
+
+    def __str__(self):
+        return f"{self.nombre_completo} - {self.marca} {self.modelo}"
+
+    class Meta:
+        verbose_name = "Formulario de Búsqueda"
+        verbose_name_plural = "Formularios de Búsqueda"
