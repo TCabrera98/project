aa54ca3359240a388e5cba92114a7b1d04062eb6
Valentin Cabrera <valentincabrera98@gmail.com>
2025-04-23 18:00:56 -0300

    add: mejoras en la logica de comparacion, agregando mensajes de error y refinando la comparacion

--- apps/vehiculo/views.py
+++ apps/vehiculo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from vehiculo.models import Vehiculo
+from django.contrib import messages
 from vehiculo.forms import ModeloFormularioBusquedaForm
 
 
@@ -66,17 +67,25 @@ def usados(request):
     return render(request, "vehiculo/usados.html", contexto)
 
 
-def formulario_comparar(request):
-    # Obtén todos los vehículos de la base de datos
-    vehiculos = Vehiculo.objects.all()
-    return render(request, 'vehiculo/formulario_comparar.html', {'vehiculos': vehiculos})
-
-
 def comparar_vehiculos(request):
     vehiculo_id_1 = request.GET.get("vehiculo_1")
     vehiculo_id_2 = request.GET.get("vehiculo_2")
 
+    # Mensaje de error si no selecciona ningun vehiculo
+    if not vehiculo_id_1 and not vehiculo_id_2:
+        messages.warning(
+            request, "Debe seleccionar para comparar.")
+        return redirect("vehiculo:formulario_comparar")
+
+    # Mensaje de error si solo selecciona un vehiculo
     if not vehiculo_id_1 or not vehiculo_id_2:
+        messages.warning(
+            request, "Debe seleccionar ambos vehiculos para comparar.")
+        return redirect("vehiculo:formulario_comparar")
+
+    # Mensaje de error si selecciona el mismo vehiculo
+    if vehiculo_id_1 == vehiculo_id_2:
+        messages.warning(request, "No se compararán dos vehiculos iguales.")
         return redirect("vehiculo:formulario_comparar")
 
     vehiculo1 = get_object_or_404(Vehiculo, id=vehiculo_id_1)
@@ -88,6 +97,12 @@ def comparar_vehiculos(request):
     }
 
     return render(request, "vehiculo/comparar_vehiculos.html", contexto)
+
+
+def formulario_comparar(request):
+    # Obtén todos los vehículos de la base de datos
+    vehiculos = Vehiculo.objects.all()
+    return render(request, 'vehiculo/formulario_comparar.html', {'vehiculos': vehiculos})
 
 
 def modal_imagenes(request, vehiculo_id):
